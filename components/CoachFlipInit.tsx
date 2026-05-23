67e907810e655395d3ca4367566c86f98a220a1b
Antonio Guerriere <antonioguerriere@gmail.com>
2026-05-24 00:03:31 +0100

    wip

--- components/CoachFlipInit.tsx
+++ components/CoachFlipInit.tsx
@@ -6,17 +6,33 @@ export default function CoachFlipInit() {
   useEffect(() => {
     const coaches = Array.from(document.querySelectorAll<HTMLElement>(".coach"));
     const pairs = coaches.map(coach => {
-      const onStart = (e: TouchEvent) => { e.preventDefault(); coach.classList.add("flipped"); };
+      let startY = 0;
+      let scrolling = false;
+
+      const onStart = (e: TouchEvent) => {
+        startY = e.touches[0].clientY;
+        scrolling = false;
+        coach.classList.add("flipped");
+      };
+      const onMove = (e: TouchEvent) => {
+        if (!scrolling && Math.abs(e.touches[0].clientY - startY) > 8) {
+          scrolling = true;
+          coach.classList.remove("flipped");
+        }
+      };
       const onEnd = () => coach.classList.remove("flipped");
-      coach.addEventListener("touchstart", onStart, { passive: false });
+
+      coach.addEventListener("touchstart", onStart, { passive: true });
+      coach.addEventListener("touchmove", onMove, { passive: true });
       coach.addEventListener("touchend", onEnd);
       coach.addEventListener("touchcancel", onEnd);
-      return { coach, onStart, onEnd };
+      return { coach, onStart, onMove, onEnd };
     });
 
     return () => {
-      pairs.forEach(({ coach, onStart, onEnd }) => {
+      pairs.forEach(({ coach, onStart, onMove, onEnd }) => {
         coach.removeEventListener("touchstart", onStart);
+        coach.removeEventListener("touchmove", onMove);
         coach.removeEventListener("touchend", onEnd);
         coach.removeEventListener("touchcancel", onEnd);
       });
